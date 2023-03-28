12349f66d8160aa82e05b1bcfd74b508f6ce6c70
rusmarch <jakal5577>
2023-03-28 18:00:59 +0300

    third commit

--- src/data/feedbackData.js
+++ src/data/feedbackData.js
@@ -1,6 +1,5 @@
 export const feedbackData = [
    {id: 1, rating: 10, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit?'},
       {id: 2, rating: 5, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, quam.'},
-      {id: 3, rating: 7, text: 'Lorem ipsum dolor sit amet consectetur adipisicing'},
-      {id: 4, rating: 3, text: 'New feedback here'}
+      //{id: 3, rating: 7, text: 'Lorem ipsum dolor sit amet consectetur adipisicing'},
 ];
